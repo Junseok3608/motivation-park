7652438dc8c551cd9d1ba16a5643419a38da3f5a
JsKim <75792069+Junseok3608@users.noreply.github.com>
2023-01-28 21:00:36 +0900

    EDIT: clock.js - add time func in greeting
    add time func in greeting

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,5 @@
 const time = document.getElementById("time");
+const greetingTime = document.getElementById("greeting-time");
 
 const getClock = function () {
   const date = new Date();
@@ -7,6 +8,7 @@ const getClock = function () {
   const seconds = String(date.getSeconds()).padStart(2, "0");
 
   time.innerText = `${hours}:${minutes}:${seconds}`;
+  greetingTime.innerText = `현재 시간은 달리기 좋은 ${hours}시 ${minutes}분`;
 };
 getClock();
 setInterval(getClock, 1000);
