97221a5cceda7f29676270a92d84fce16254dfe0
JsKim <75792069+Junseok3608@users.noreply.github.com>
2023-01-29 19:00:52 +0900

    ADD: running.js
    add running motive todos

---
+++ js/running.js
@@ -0,0 +1,60 @@
+const runningForm = document.getElementById("running-form");
+const runningInputKm = document.getElementById("running-input__km");
+const runningInputTime = document.getElementById("running-input__time");
+const runningList = document.querySelector(".running-list");
+
+const RUNNING_KEY = "running";
+
+let runs = [];
+
+const saveRuns = function () {
+  localStorage.setItem(RUNNING_KEY, JSON.stringify(runs));
+};
+
+const deleteRun = function (e) {
+  const li = e.target.parentElement;
+  li.remove();
+  runs = runs.filter((run) => run.id !== parseInt(li.id));
+  saveRuns();
+};
+
+const paintRun = function (newRun) {
+  const li = document.createElement("li");
+  li.id = newRun.id;
+  const spankm = document.createElement("span");
+  spankm.innerText = `오늘의 목표! ${newRun.textkm}km`;
+  const spantime = document.createElement("span");
+  spantime.innerText = `Time Limit! ${newRun.texttime}`;
+  const btn = document.createElement("button");
+  btn.innerHTML = `☑`;
+  btn.addEventListener("click", deleteRun);
+  li.appendChild(spankm);
+  li.appendChild(spantime);
+  li.appendChild(btn);
+  runningList.appendChild(li);
+};
+
+const handleRunSubmit = function (e) {
+  e.preventDefault();
+  const newRunKm = runningInputKm.value;
+  const newRunTime = runningInputTime.value;
+  runningInputKm.value = "";
+  runningInputTime.value = "";
+  const newRunObj = {
+    textkm: newRunKm,
+    texttime: newRunTime,
+    id: Date.now(),
+  };
+  runs.push(newRunObj);
+  paintRun(newRunObj);
+  saveRuns();
+};
+
+runningForm.addEventListener("submit", handleRunSubmit);
+
+const savedRuns = localStorage.getItem(RUNNING_KEY);
+if (savedRuns !== null) {
+  const parsedRuns = JSON.parse(savedRuns);
+  runs = parsedRuns;
+  parsedRuns.forEach(paintRun);
+}
